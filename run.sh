f984cbc4403196d3a00d908d5e4b7eac41dea4e3
Justin Wozniak <woz@anl.gov>
2021-02-23 13:43:53 -0600

    Add initial stuff

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+set -eu
 
 print "BUILD SAMPLES RUN.SH"
 
@@ -10,26 +11,46 @@ date-nice()
 
 print "START: $( date-nice )"
 
-print "PWD: $( /bin/pwd )"
+TOP=$( /bin/pwd )
+print "PWD: $TOP"
 
-exit 
+set -x
 
-wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+mkdir -pv downloads sfw
+ANACONDA_SH=Anaconda3-2020.11-Linux-x86_64.sh
+cd downloads
+if [[ ! -f $ANACONDA_SH ]] {
+  wget https://repo.anaconda.com/archive/$ANACONDA_SH
+}
+
+cd ..
+cd sfw
+
+if [[ ! -d anaconda3 ]] {
+  ../downloads/$ANACONDA_SH -bs -p $TOP/sfw/anaconda3
+}
+
+PATH=$TOP/sfw/anaconda3/bin:$PATH
 
 conda install r
 
-git clone https://github.com/spack/spack.git
+if [[ ! -d spack ]] {
+  git clone https://github.com/spack/spack.git
+}
+
+exit
 
 # Substitute into etc/spack/packages.yaml
+
 
 # Check using correct Python
 # e.g. spack spec python => "python@3.6.2%gcc@7.1.0+..."
 spack spec python
 spack find --paths python # => /home/woz/Public/sfw/bebop/anaconda3
 
-spack install ant autoconf automake jdk libtool m4 pcre swig zsh 
+spack install ant autoconf automake jdk libtool m4 pcre swig zsh
 # Check that using system installations
-spack find --paths zsh 
+spack find --paths zsh
 
 # Check that Spack is using correct installations
 python mpi tcl r
